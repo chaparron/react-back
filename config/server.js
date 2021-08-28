b9a61fa40e4fc4d788c7db9b70ae43a5617ee291
chaparron <jony13gm@gmail.com>
2021-08-28 19:37:03 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e1d4fbf89e5450ebc66650f18faf38ae'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '60225c99-b205-4a59-aa99-229e28f03a36'
+};
